b770c913e3990ca78f819b8de9b18ffacdcd74ec
Claudia González <claudia.glezg@gmail.com>
2024-01-17 12:06:29 +0100

    create repository and first file

---
+++ primerospasos.php
@@ -0,0 +1,93 @@
+<?php
+/* /* // Declara una variable por cada tipo de dato y asignar un valor. Muestra el valor de esa variable en consola y en pantalla (Averigua cómo imprimir el valor
+booleano (que imprima true o false), un array y un valor nulo (que imprima NULL) */
+
+  $nombre = "Gala";
+//  var_dump($nombre);
+ /*$numero = 79;
+// var_dump($numero);
+$numeroFloat = 43.2;
+// var_dump($numeroFloat);
+ $perroLadra = true;
+// var_dump($perroLadra);
+ $personajesSimpsons = array (
+    "Homer",
+    "Lisa",
+    "Maggie",
+    "Bart",
+    "Marge",
+ );  */
+// var_dump($personajesSimpsons);
+// $nulo = null;
+// var_dump($nulo);
+
+/* echo $nombre . "<br>";
+echo $numero . "<br>";
+echo $numeroFloat . "<br>";
+echo $perroLadra . "<br>";
+print_r($personajesSimpsons)  . "<br>";  //Con el echo no sale el valor del Array
+echo $nulo . "<br>";  */
+
+// ¿Qué tipo de dato son las siguientes variables?
+//Fácil
+/* $nombre = "Luna";
+$apellido = "Lovegood";
+$edad = 42;
+$Ravenclaw = true; */
+
+//var_dump($nombre);
+//var_dump($apellido );
+//var_dump($edad);
+//var_dump($Ravenclaw); */
+
+//Medio
+/* $promedio = (8 + 9.5 + 9 + 10 + 8) / 5;
+$nombre_completo = $nombre . " " .
+$apellido;
+$presento_examen = (bool) 1; */
+
+/* var_dump($promedio);
+var_dump($nombre_completo);
+var_dump($apellido);
+var_dump($presento_examen); */
+
+//Avanzado
+/* $numero_preguntas = 5 + "5";
+$numero_respuestas = "5" + 5;
+$promedio_maximo =
+$numero_respuestas / 1.0;
+$nargles = 3 + "5 nargles";
+
+var_dump($numero_preguntas);
+var_dump($numero_respuestas);
+var_dump($promedio_maximo);
+var_dump($nargles); */
+
+// Suponiendo estas variables
+/* $es_un_michi_grande = true;
+$le_gusta_comer = true;
+$sabe_volar = false;
+$tiene_2_patas = false;
+// ¿Cuál es el resultado según las tablas de verdad?
+$es_un_michi_grande && $le_gusta_comer;
+$es_un_michi_grande || $sabe_volar;
+$sabe_volar || $tiene_2_patas;
+!$le_gusta_comer;
+!$le_gusta_comer || $es_un_michi_grande;
+
+var_dump($es_un_michi_grande && $le_gusta_comer);
+var_dump($es_un_michi_grande || $sabe_volar);
+var_dump($sabe_volar || $tiene_2_patas);
+var_dump(!$le_gusta_comer);
+var_dump(!$le_gusta_comer || $es_un_michi_grande); */
+
+
+
+
+
+
+
+
+
+
+?>
